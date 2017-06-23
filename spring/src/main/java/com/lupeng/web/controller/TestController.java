17270f5742598cdc88ffd55940fdd909b70445c4
lupeng <lupeng@credithundred.com>
2017-06-23 14:45:47 +0800

    添加redis

--- spring/src/main/java/com/lupeng/web/controller/TestController.java
+++ spring/src/main/java/com/lupeng/web/controller/TestController.java
@@ -1,10 +1,12 @@
 package com.lupeng.web.controller;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -20,14 +22,18 @@ public class TestController {
     @Autowired
     public TestService testService;
     
+    @Resource(name = "redisTemplate")
+    protected ValueOperations<String, Object> redisCache;
+    
     @RequestMapping("test")
     public String test(HttpServletRequest request){
-        System.err.println("testtttttt");
         User user = new User();
         user.setName("lupeng");
         System.err.println(testService.savaUser(user));
         User users =  testService.findUserById(1L);
         System.err.println(users);
+        redisCache.set("userId",users.getId() );
+        System.err.println(redisCache.get("userId"));
         request.setAttribute("user",users);
         return "index";
     }
